c3573eec04fb11c0b63a5fdce4e1a0308e00d982
Abby Roberg <abbyroberg@gmail.com>
2021-09-14 15:10:18 -0500

    user routes

---
+++ models/User.js
@@ -0,0 +1,61 @@
+const { Model, DataTypes } = require('sequelize')
+const sequelize = require('../config/connection')
+
+// create User model
+class User extends Model{}
+
+// define table columns and configuration
+User.init(
+    {
+        // define an id column
+        id: {
+            // use the Sequelize DataTypes object provide what type of data it is
+            type: DataTypes.INTEGER,
+            // SQL NOT NULL option
+            allowNull: false,
+            // make id the primary key
+            primaryKey: true,
+            // turn on auto increment
+            autoIncrement: true
+        },
+        // define a username column
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        // define an email column
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            // there cannot be any duplicate email values in this table
+            unique: true,
+            // validate not null data
+            validate: {
+                isEmail: true
+            }
+        },
+        // define a password column
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                // password must be at least 4 characters long
+                len: [4]
+            }
+        }
+    },
+    {
+        // pass in our imported sequelize connection
+        sequelize,
+        // don't automatically create createdAt/updatedAt timestamp fields
+        timestamps: false,
+        // don't pluralize name of database table
+        freezeTableName: true,
+        // use underscores instead of camel_casing
+        underscored: true,
+        // make it so our model name stays lowercase in the database
+        modelName: 'user'
+    }
+)
+
+module.exports = User
