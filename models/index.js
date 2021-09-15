53905120afa36270fb1ddbb77d3a8cfdaddec7ac
Abby Roberg <abbyroberg@gmail.com>
2021-09-15 12:20:54 -0500

    voting system

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require('./User')
 const Post = require('./Post')
+const Vote = require('./Vote')
 
 // create association (1 user has many posts)
 User.hasMany(Post, {
@@ -9,4 +10,37 @@ Post.belongsTo(User, {
     foreignKey: 'user_id'
 })
 
-module.exports = { User, Post }
+// create association (through votes)
+User.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'user_id'
+})
+  
+Post.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id'
+})
+
+// create association (1 vote to 1 user)
+Vote.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+// create association (1 vote to 1 post)
+Vote.belongsTo(Post, {
+    foreignKey: 'post_id'
+})
+
+// create association (1 user has many votes)
+User.hasMany(Vote, {
+    foreignKey: 'user_id'
+})
+
+// create association (1 post has many votes)
+Post.hasMany(Vote, {
+    foreignKey: 'post_id'
+})
+
+module.exports = { User, Post, Vote }
